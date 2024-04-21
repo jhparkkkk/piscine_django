f9aa2e078994146d1a910de2ea4d270bd41bcc0c
Jee <jeehyun24park@gmail.com>
2024-04-21 13:09:26 +0200

    d01 ex00: added doc

--- 0.1.Starting/ex00/var.py
+++ 0.1.Starting/ex00/var.py
@@ -1,5 +1,9 @@
 def my_var():
+    """
+    Prints the type of various objects contained in a list
+    """
     var_list = [42, "42", "quarante-deux", 42.0, True, [42], {42: 42}, (42,), set()]
+    
     for item in var_list:
         print(f"{item} est de type {type(item)}")
 
