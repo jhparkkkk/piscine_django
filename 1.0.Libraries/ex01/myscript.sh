09ef6d12665d57c85246a21895564ec0451c95c9
Jee hyun PARK <jeehyun.park@hotmail.com>
2025-07-16 11:06:17 +0200

    exec script only if success

--- 1.0.Libraries/ex01/myscript.sh
+++ 1.0.Libraries/ex01/myscript.sh
@@ -2,6 +2,4 @@
 
 pip --version
 
-pip install git+https://github.com/jaraco/path.git --target=./local_lib --upgrade --log install.log
-
-python3 my_program.py
+pip install git+https://github.com/jaraco/path.git --target=./local_lib --upgrade --log install.log && python3 my_program.py
